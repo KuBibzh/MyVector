0397605755e21dc5a6f50585025fa04ef6f38128
baozhenhua <baozhenhua@hehe.com>
2018-09-16 17:41:22 +0800

    增加新的描述

--- MyVector.h
+++ MyVector.h
@@ -21,6 +21,7 @@ class MyVector{
 		int cap;
 		enum{INCREMENT=50};
 	private:
+		//快速排序的实现
 		void sort(T* array,int len,int(*cmp)(const T&,const T&)){
 			if(len<2) return;
 			int i=0,j=len-1;
@@ -37,18 +38,22 @@ class MyVector{
 			sort(array+i+1,len-i-1,cmp);
 		}
 	public:
+		//无参构造函数，容器大小默认
 		MyVector(){
 			cap=100;
 			len=0;
 			array=new T[cap];
 		}
+		//用户传入容器大小的带参构造函数
 		MyVector(int cap):cap(cap){
 			len=0;
 			array=new T[cap];
 		}
+		//析构函数
 		~MyVector(){
 			delete[] array;
 		}
+		//向头部插入
 		void push_front(const T& data){
 			if(len==cap){
 				cap+=INCREMENT;
@@ -63,6 +68,7 @@ class MyVector{
 			array[0]=data;
 			len++;
 		}
+		//向尾部插入
 		void push_back(const T& data){
 			if(len==cap){
 				cap+=INCREMENT;
@@ -74,6 +80,7 @@ class MyVector{
 			array[len]=data;
 			len++;
 		}
+		//朝指定位置插入
 		void insert(int pos,const T& data){
 			if(len==cap){
 				cap+=INCREMENT;
@@ -90,6 +97,7 @@ class MyVector{
 			array[pos]=data;
 			len++;
 		}
+		//移除跟指定数据相同的容器项
 		bool remove(const T& data){
 			if(cap>5*len){
 				cap=2*len;
@@ -108,6 +116,7 @@ class MyVector{
 			}
 			return false;
 		}
+		//查找指定数据的位置
 		int find(const T& data){
 			for(int i=0;i<len;i++){
 				if(array[i]==data) return i;
@@ -115,21 +124,26 @@ class MyVector{
 			throw 1;
 
 		}
+		//判断容器是否为空
 		bool empty(){
 			return !len;
 		}
+		//判断容器数据长度
 		int size(){
 			return len;
 		}
+		//清空容器
 		void clear(){
 			len=0;
 		}
+		//遍历
 		void visit(int(*show)(const T&)){
 			for(int i=0;i<len;i++){
 				if(!show(array[i]))
 					break;
 			}
 		}
+		//数据遍历
 		void reverse(){
 			for(int i=0;i<len/2;i++){
 				T temp=array[i];
@@ -137,10 +151,11 @@ class MyVector{
 				array[len-i-1]=temp;
 			}
 		}
+		//数据排序
 		void sort(int(*cmp)(const T&,const T&)){
 			sort(array,len,cmp);
 		}
-		
+		//根据传入参数访问数据
 		T& operator[](int n){
 			if(n<0 || n>=len) throw 1;
 			return array[n];
--- README.md
+++ README.md
@@ -3,3 +3,22 @@
 ## 版本
 ### v1.00
 实现了Vector容器类模板的功能
+
+## 函数展示
+		void sort(T* array,int len,int(*cmp)(const T&,const T&));
+		MyVector();
+		MyVector(int cap);
+		~MyVector();
+		void push_front(const T& data);
+		void push_back(const T& data);
+		void insert(int pos,const T& data);
+		bool remove(const T& data);
+		int find(const T& data);
+		bool empty();
+		int size();
+		void clear();
+		void visit(int(*show)(const T&));
+		void reverse();
+		void sort(int(*cmp)(const T&,const T&));
+
+		T& operator[](int n);
